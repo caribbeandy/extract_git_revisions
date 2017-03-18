9d65c73a429ef1815aece222cfe54d555823f684
Andrew Palmer <avg.palmer@gmail.com>
2017-03-18 19:54:24 +0000

    Update

--- gen_snapshots.sh
+++ gen_snapshots.sh
@@ -1,39 +1,62 @@
 #/bin/bash
 
+	beforeDate="Jan 17 2016"
+	afterDate="May 17 2016"
 
 	## for each repository
-	repoName=caliper
+	for repoName in * ; do
 
-	beforeDate="Jan 17 2014"
+        if [ -f "$repoName" ]; then
+            continue
+        fi
 
-	# move into directory
-	cd $repoName 
+        #repoName=caliper
 
-	# move everything into the after dir
-	mkdir ${repoName}_after
-    mv .[a-zA-Z0-9]* ${repoName}_after
-	mv * ${repoName}_after
+        # move into directory
+        cd $repoName 
 
-	# create before dir and copy everything into it
-	mkdir ${repoName}_before
-	cd ${repoName}_after
-    cp -rf .[a-zA-Z0-9]* ${repoName}_before
-	cp -rf . ../${repoName}_before/
+        # move everything into the after dir
+        mkdir ${repoName}_after
+        mv .[a-zA-Z0-9]* ${repoName}_after > /dev/null 2>&1
+        mv * ${repoName}_after > /dev/null 2>&1
 
-    exit 0
+        # create before dir and copy everything into it
+        mkdir ${repoName}_before
+        cd ${repoName}_after
 
-	# go to before dir and check for the snapshot
-	cd ../${repoName}_before
-	git rev-list -1 --before=$beforeDate master > rev_before.txt
+        cp -rf .[a-zA-Z0-9]* ../${repoName}_before
+        cp -rf * ../${repoName}_before/
 
-    # if repo doesn't go back that far in time, record project name, and exit
-	if [ ! -s rev_before.txt ]; then
-	    echo "File not found, or it is empty"
-        echo "Repository doesn't go back to the date specified"
-        echo "Exiting"
-        # if rev_before empty, write name to dir, exit
-        exit 1 
-	fi
+        # go to before dir and check for the snapshot
+        cd ../${repoName}_before
+        git rev-list -1 --before="${beforeDate}" master > rev_before.txt
 
-	#git rev-list -1 --before="Jan 17 2014" master > rev_after
+        ### Also need to account for initial commits
 
+        # if repo doesn't go back that far in time, record project name, and exit
+        if [ ! -s rev_before.txt ]; then
+            echo "File not found, or it is empty"
+            echo "Repository doesn't go back to the date specified"
+            echo "Exiting"
+            # if rev_before empty, write name to dir, exit
+
+            exit 1 
+        fi
+
+        # revert to the snapshot
+        git checkout `cat rev_before.txt` > /dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            echo Checked out the before snapshot
+        fi
+
+        # start on the after snapshot
+        cd ../${repoName}_after
+        #git rev-list -1 --before="Jan 17 2014" master > rev_after
+        git checkout `git rev-list -1 --before="${afterDate}" master` > /dev/null 2>&1
+
+        if [ $? -eq 0 ]; then
+            echo Checked out the after snapshot
+        fi
+
+        echo Successfully checked out project $repoName
+	done
