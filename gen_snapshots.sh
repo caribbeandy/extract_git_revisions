fdded67e9e1f86ee763dbade34d0367f6850b91e
Andrew Palmer <avg.palmer@gmail.com>
2017-04-02 22:37:36 +0000

    Update

--- gen_snapshots.sh
+++ gen_snapshots.sh
@@ -1,57 +1,69 @@
-#/bin/bash
+#!/bin/bash
 
-	beforeDate="Jan 1 2012"
-	afterDate="Jan 1 2013"
+	#beforeDate="Jan 1 2012"
+	#afterDate="Jan 1 2013"
 
     mainDir="/home/a9palmer/iclones_test"
+    dates=( "Jan 1 2012" )
 
-    rm ${mainDir}/failed.txt 
-    rm ${mainDir}/succeeded.txt 
+    rm ${mainDir}/failed.txt > /dev/null 2>&1
+    rm ${mainDir}/succeeded.txt > /dev/null 2>&1
 
+    i=0
 	## for each repository
 	for repoName in `find ${mainDir} -maxdepth 1 -mindepth 1 -type d -printf '%f\n'` ; do
 
+        #### Preparation
+
         cd $mainDir
 
-        # move into directory
+        # cd into directory
         cd $repoName 
 
-        # move everything into the after dir
-        mkdir ${repoName}_after
-        mv .[a-zA-Z0-9]* ${repoName}_after > /dev/null 2>&1
-        mv * ${repoName}_after > /dev/null 2>&1
+        origFolder="${repoName}_original"
 
-        # create before dir and copy everything into it
-        mkdir ${repoName}_before
-        cd ${repoName}_after
+        # move everything into the newest dir
+        mkdir $origFolder
+        mv .[a-zA-Z0-9]* $origFolder > /dev/null 2>&1
+        mv * $origFolder > /dev/null 2>&1
+        #todo: change to move ****
 
-        cp -rf .[a-zA-Z0-9]* ../${repoName}_before
-        cp -rf * ../${repoName}_before/
+        ## end preparation
 
-        # go to before dir and check for the snapshot
-        cd ../${repoName}_before
-        git rev-list -1 --before="${beforeDate}" master > rev_before.txt
+        for date in "${dates[@]}"
+        do
 
-        ### Also need to account for initial commits
+            # create dir for the date and copy everything into it
+            mkdir "${date}"
+            cd $origFolder
 
-        # if repo doesn't go back that far in time, record project name, and exit
-        if [ ! -s rev_before.txt ]; then
-            echo "$(tput setaf 1)Repository [$repoName] doesn't go back to the date specified[$repoName]$(tput sgr0)"
-            echo $repoName >> ${mainDir}/failed.txt 
-        else
-            echo $repoName >> ${mainDir}/succeeded.txt 
-        fi
+            cp -rf .[a-zA-Z0-9]* ../"${date}"
+            cp -rf * ../"${date}"/
 
-        # revert to the snapshot
-        git checkout `cat rev_before.txt` > /dev/null 2>&1
+            # go to date dir and check for the snapshot
+            cd ../"${date}"
+            git rev-list -1 --before="${date}" master > rev_"${date}".txt
 
-   #     if [ $? -eq 0 ]; then
-   #        echo Checked out the before snapshot
-   #     fi
+            ### TODO: Also need to account for initial commits
 
-        # start on the after snapshot
-        cd ../${repoName}_after
-        git checkout `git rev-list -1 --before="${afterDate}" master` > /dev/null 2>&1
+            # if repo doesn't go back that far in time, record project name, and exit
+            if [ ! -s "rev_${date}.txt" ]; then
+                echo "$(tput setaf 1)Repository [$repoName] doesn't go back to the date specified[$date]$(tput sgr0)"
+                echo ${repoName}_${date} >> ${mainDir}/failed.txt 
+            else
+                echo ${repoName}_${date} >> ${mainDir}/succeeded.txt 
+            fi
 
-        echo "$(tput setaf 2)Successfully checked out project [$repoName]$(tput sgr0)"
+            # checkout the snapshot
+            git checkout `cat rev_"${date}".txt` > /dev/null 2>&1
+            rm rev_"${date}".txt
+
+       #     if [ $? -eq 0 ]; then
+       #        echo Checked out the before snapshot
+       #     fi
+
+            echo "$(tput setaf 2)Successfully checked out project [$repoName] for date [$date]$(tput sgr0)"
+
+            i=$((i + 1))
+        done
 	done
